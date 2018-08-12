c500b474d0eea2da29ec6e09a4e673f791cfb4af
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-08-12 18:47:49 +0100

    Introducing class DB_Upgrade

--- spec/db_upgrade_spec.rb
+++ spec/db_upgrade_spec.rb
@@ -2,34 +2,34 @@
 
 require_relative '../lib/DB_upgrade.rb'
 
-describe 'DB upgrade' do
+describe DBUpgrade do
+  let(:upgrade) { DBUpgrade.new }
 
   describe '#lookup_current_db_version' do
     it 'Looks up the current version number' do
-      expect(lookup_current_db_version).to eq 45
+      expect(upgrade.lookup_current_db_version).to eq 45
     end
   end
 
   describe '#scan_scripts_names' do
     it '# scans the scripts folder and returns a collection of script names' do
       dir_name = 'db/upgrade_scripts'
-      expect(scan_scripts_names(dir_name))
+      expect(upgrade.scan_scripts_names(dir_name))
         .to include('045.createtable.sql', '049.createtable.sql', '011createtable.sql')
     end
   end
-
 
   describe '#db_status' do
     it 'the current version is equal to the highest of the scripts numbers' do
       allow(Dir).to receive(:entries)
                       .and_return(%w[045.createtable.sql 011createtable.sql])
       dir_name = 'db/upgrade_scripts'
-      expect(db_status(dir_name)).to eq 'DB up to date!'
+      expect(upgrade.db_status(dir_name)).to eq 'DB up to date!'
     end
 
     it 'and the current version is lower than the highest of the scripts numbers' do
       dir_name = 'db/upgrade_scripts'
-      expect(db_status(dir_name)).to eq 'Upgrade your DB!'
+      expect(upgrade.db_status(dir_name)).to eq 'Upgrade your DB!'
     end
   end
 end
