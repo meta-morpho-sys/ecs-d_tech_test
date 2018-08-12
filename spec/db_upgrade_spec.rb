784f7da3006686e4a17fc2f12d01012a1236e4e2
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-08-12 13:15:22 +0100

    Revert "Better naming"
    This reverts commit 64ef2e1

--- spec/db_upgrade_spec.rb
+++ spec/db_upgrade_spec.rb
@@ -4,15 +4,10 @@ require_relative '../lib/DB_upgrade.rb'
 
 describe 'DB upgrade' do
 
-  describe '#lookup_current_db_version' do
+  describe '#lookup_db_version' do
     it 'Looks up the current version number' do
-      expect(lookup_current_db_version).to eq 45
+      expect(lookup_db_version).to eq 45
     end
   end
 
-  describe '#'
-
-  xdescribe '#compare_version_nums'
-
-
- end
+end
