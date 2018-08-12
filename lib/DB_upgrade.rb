f202d1e0ba96fbc3294b4a650494b82d156443bb
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-08-12 22:14:31 +0100

    Design update and refactoring for the class DBUpgrade to accept parameters as specified in the last bullet point of the test requirements

--- lib/DB_upgrade.rb
+++ lib/DB_upgrade.rb
@@ -3,23 +3,32 @@
 require_relative '../db/sequel_setup'
 
 class DBUpgrade
+  VERSION_TABLE = DB[:versionTable]
+
+  def initialize(sql_scripts_dir, db_username, db_host, db_name, db_password)
+    @sql_scripts_dir = sql_scripts_dir
+    @db_username = db_username
+    @db_host = db_host
+    @db_name = db_name
+    @db_password = db_password
+  end
 
   def lookup_current_db_version
-    DB[:versionTable].map(:version).first
+    VERSION_TABLE.map(:version).first
   end
 
-  def scan_scripts_names(path)
-    Dir.entries(path).select { |f| File.file?(File.join(path, f)) }
-  end
-
-  def db_status(path)
-    current_bd_v = lookup_current_db_version
-    highest_script_v = scan_scripts_names(path).max.scan(/\d+/).first.to_i
-    if highest_script_v > current_bd_v
-      'Upgrade your DB!'
+  def upgrade_db
+    highest_script_v = scan_scripts_names.max.scan(/\d+/).first.to_i
+    if highest_script_v > lookup_current_db_version
+      DB.run File.read('/Users/astarte/TechTests/ECS-D/db/upgrade_scripts/049.createtable.sql')
+      VERSION_TABLE.update(version: highest_script_v)
     else
       'DB up to date!'
     end
   end
-end
 
+  def scan_scripts_names
+    Dir.entries(@sql_scripts_dir)
+      .select { |f| File.file?(File.join(@sql_scripts_dir, f)) }
+  end
+end
--- spec/db_upgrade_spec.rb
+++ spec/db_upgrade_spec.rb
@@ -2,34 +2,36 @@
 
 require_relative '../lib/DB_upgrade.rb'
 
-describe DBUpgrade do
-  let(:upgrade) { DBUpgrade.new }
+describe DBUpgrade, :db do
+  let(:upgrader) { DBUpgrade.new('db/upgrade_scripts', 'root', 'localhost', 'test', 'yuliya') }
 
   describe '#lookup_current_db_version' do
     it 'Looks up the current version number' do
-      expect(upgrade.lookup_current_db_version).to eq 45
+      expect(upgrader.lookup_current_db_version).to eq 49
     end
   end
 
   describe '#scan_scripts_names' do
     it '# scans the scripts folder and returns a collection of script names' do
-      dir_name = 'db/upgrade_scripts'
-      expect(upgrade.scan_scripts_names(dir_name))
+      expect(upgrader.scan_scripts_names)
         .to include('045.createtable.sql', '049.createtable.sql', '011createtable.sql')
     end
   end
 
-  describe '#db_status' do
+  describe '#upgrade_db' do
     it 'the current version is equal to the highest of the scripts numbers' do
-      allow(Dir).to receive(:entries)
-                      .and_return(%w[045.createtable.sql 011createtable.sql])
-      dir_name = 'db/upgrade_scripts'
-      expect(upgrade.db_status(dir_name)).to eq 'DB up to date!'
+      allow(Dir)
+        .to receive(:entries)
+        .and_return(%w[045.createtable.sql 011createtable.sql])
+      expect(upgrader.upgrade_db).to eq 'DB up to date!'
     end
 
     it 'and the current version is lower than the highest of the scripts numbers' do
-      dir_name = 'db/upgrade_scripts'
-      expect(upgrade.db_status(dir_name)).to eq 'Upgrade your DB!'
+      allow(Dir)
+        .to receive(:entries)
+              .and_return(%w[045.createtable.sql 011createtable.sql 049.createtable.sql])
+      upgrader.upgrade_db
+      expect(upgrader.lookup_current_db_version).to eq 49
     end
   end
 end
