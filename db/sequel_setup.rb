70b1f11977d5204d174a146ca018ab593fdb309c
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-08-12 22:09:09 +0100

    Tidying up

--- db/sequel_setup.rb
+++ db/sequel_setup.rb
@@ -2,12 +2,7 @@
 
 require 'sequel'
 
-# ENV['DATABASE_URL'] ||= "./app/db/#{ENV.fetch('RACK_ENV', 'development')}.db"
-ENV['DATABASE_URL'] = './app/db/tech_test.db'
-
-DB = Sequel.mysql2(:host => 'localhost', :database => 'tech_test', :user => "root", :password => "yuliya")
-
-# DB = Sequel.mysql2(ENV['DATABASE_URL'])
+DB = Sequel.mysql2(host: 'localhost', database: 'tech_test', user: 'root', password: 'yuliya')
 
 unless DB.table_exists? 'versionTable'
   DB.create_table :versionTable do
