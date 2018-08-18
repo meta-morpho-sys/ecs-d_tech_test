8753f54893d2c6cb680beeeb72caacde0b9d1a93
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-08-18 16:57:26 +0100

    DB instantiation is done through env vars.

--- lib/DB_upgrade2.rb
+++ lib/DB_upgrade2.rb
@@ -1,44 +1,53 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative '../db/sequel_setup'
+DB = Sequel.mysql2(host: ENV['host'], database: ENV['db'], user: ENV['user'], password: ENV['pwd'])
 
-
-VERSION_TABLE = DB[:versionTable]
-
-def current_db_version
-  VERSION_TABLE.map(:version).first
-end
-
-def upgrade_db(dir, user, host, database, pswd)
-  if get_highest_script_num(dir) > current_db_version
-    # Updates DB
-    # Run those files that are higher version than Current DB version
-    files_to_run = select_higher_versions(dir)
-    files_to_run.each { |file| DB.run File.read dir + '/' + file }
-    VERSION_TABLE.update(version: get_highest_script_num(dir))
-  else
-    'DB up to date!'
+unless DB.table_exists? 'versionTable'
+  DB.create_table :versionTable do
+    Integer :version
+    print 'Table created'
   end
 end
 
-def select_higher_versions(dir)
-  scan_scripts_names(dir).select do |name|
-    version(name) > current_db_version
+class DatabaseYuliya
+  def initialize(dir)
+    @dir = dir
   end
-end
 
-def get_highest_script_num(dir)
-  get_versions(scan_scripts_names(dir)).max
-end
+  def db_version
+    DB[:versionTable].map(:version).first
+    p 'got here'
+  end
+
+  def db_version=(version)
+    DB[:versionTable].update(version: version)
+  end
+
+  def upgrade_db
+    scripts = Scrypts.look_up(@dir)
+    if get_highest_script(dir) > db_version
+      files_to_run = select_higher_versions(dir)
+      files_to_run.each { |file| db.run File.read dir + '/' + file }
+      db_version = 49
+    else
+      puts 'DB up to date!'
+    end
+  end
+
+  def select_higher_versions(dir)
+    scan_scripts_names(dir).select do |name|
+      version(name) > db_version
+    end
+  end
+
+  def get_highest_script(dir)
+    get_versions(Scrypts.look_up(dir)).max
+  end
 
 
 # Returns a collection of script versions
-def get_versions(strings)
-  strings.map { |s| version(s) }
-end
-
-# returns the version of the script
-def version(str)
-  str[/\d+/].to_i
+  def get_versions(strings)
+    strings.map { |s| version(s) }
+  end
 end
