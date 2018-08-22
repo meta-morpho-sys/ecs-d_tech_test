fdbe742df878b42ed231d3a030a88bdc915a047d
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-08-22 14:03:06 +0100

    Test DB setup.

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -1,10 +1,34 @@
 # frozen_string_literal: true
 
+require 'rspec'
+require 'sequel'
+require_relative '../../lib/logging'
+
+LOGGER = my_logger
+
+DB = Sequel.mysql2(host: 'localhost', database: 'test', user: 'root', password: 'yuliya')
+
+
+unless DB.table_exists? 'versionTable'
+  DB.create_table :versionTable do
+    Integer :version
+    LOGGER.info '> Version table for test created'
+  end
+end
+versions = DB[:versionTable]
+versions.insert(version: 0)
+
 RSpec.configure do |c|
-   # The following will roll back the transactions
+  c.before(:suite) do
+    puts '> Cleaning databases.'
+    versions.truncate
+  end
+
+  # setup DatabaseY instance
+  # The following will roll back the transactions
   # after each example has been run.
   c.around(:example, :db) do |example|
-    puts 'Rolling back transaction'
+    puts '> Rolling back transaction'
     DB.transaction(rollback: :always) { example.run }
   end
 end
