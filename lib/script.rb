dc65907b2466c4875212cca79d17ac42a9a50812
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-08-22 13:59:11 +0100

    Script class with tests

---
+++ lib/script.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+Script = Struct.new(:dir, :file_name)
+
+# Returns the version, file_path and contents of a given script
+class Script
+  # returns the version of the script
+  def version
+    file_name[/\d+/].to_i
+  end
+
+  # returns a string with the file_path
+  def file_path
+    File.join(dir, file_name)
+  end
+
+  def read
+    File.read file_path
+  end
+end
+
+# Accesses a given dir and returns an array of scrypts
+class Scripts
+  def self.look_up(dir)
+    discard_dirs(dir)
+      .map { |f| Script.new(dir, f) }
+  end
+
+  def self.discard_dirs(dir)
+    Dir.entries(dir).reject { |e| File.directory? e }
+  end
+end
--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -2,30 +2,30 @@
 
 require_relative '../lib/script'
 
-describe Scrypts do
+describe Scripts do
   it 'finds scripts in given directory' do
     expect(Dir).to receive(:entries)
       .with('db/upgrade_scripts')
       .and_return(%w[045.createtable.sql 011createtable.sql 049.createtable.sql])
-    Scrypts.look_up('db/upgrade_scripts')
+    Scripts.look_up('db/upgrade_scripts')
   end
 
   it 'returns a list of scripts for directory' do
-    scripts = Scrypts.look_up('db/upgrade_scripts')
+    scripts = Scripts.look_up('db/upgrade_scripts')
     file_names = scripts.map(&:file_name)
     expect(file_names).to eq(%w[045.createtable.sql 011createtable.sql 049.createtable.sql])
   end
 end
 
-describe Scrypt do
-  let(:scrypt) { Scrypt.new('db/upgrade_scripts', '049.createtable.sql') }
+describe Script do
+  let(:script) { Script.new('db/upgrade_scripts', '049.createtable.sql') }
 
   it 'finds the file path' do
-    expect(scrypt.file_path). to eq 'db/upgrade_scripts/049.createtable.sql'
+    expect(script.file_path). to eq 'db/upgrade_scripts/049.createtable.sql'
   end
 
   it 'reads the file contents' do
-    expect(scrypt.read). to eq <<~EOF
+    expect(script.read). to eq <<~EOF
       CREATE TABLE test (
         participant VARCHAR(20),
         age INTEGER(2),
@@ -36,7 +36,7 @@ describe Scrypt do
 
   describe '#version' do
     it 'extracts the script numbers from the file names' do
-      expect(scrypt.version).to eq 49
+      expect(script.version).to eq 49
     end
   end
 end
