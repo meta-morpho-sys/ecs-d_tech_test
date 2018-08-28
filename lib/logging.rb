73cb6f020355f767b3106f1f2c752a6a05271a07
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-08-28 17:22:01 +0100

    More detailed settings for Logger

--- lib/logging.rb
+++ lib/logging.rb
@@ -2,6 +2,14 @@
 
 require 'ansi/logger'
 
-def my_logger
-  ANSI::Logger.new(STDOUT)
+# Logger Wrapper that gives global access to Logger.
+class MyLogger
+  def self.log
+    @logger = ANSI::Logger.new($stdout) if @logger.nil?
+    @logger.formatter = proc do |severity, datetime, _progname, msg|
+      date_format = datetime.strftime('%Y/%m/%d %H:%M:%S')
+      "#{date_format} -- #{severity}: #{msg}\n"
+    end
+    @logger
+  end
 end
